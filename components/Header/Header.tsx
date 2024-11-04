503c6b15a0052728c9041ae1b6f3d03a4e9d693c
bashartech <144168218+bashartech@users.noreply.github.com>
2024-11-05 00:37:05 +0500

    first commit

---
+++ components/Header/Header.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Header() {
+    return (
+      <div className="h-[134px] w-screen bg-[#A29875] flex justify-start items-center">
+        <h1 className="header text-4xl sm:text-5xl md:text-6xl lg:text-[75px] text-white ml-4 sm:ml-[36px]">
+          MANZZARI
+        </h1>
+      </div>
+    );
+  }
